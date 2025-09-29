a9713b7e0d16a3566b052a39541cf1d78263c8e7
Abhi Krishan <itsme.abhi558@gmail.com>
2025-09-29 22:32:30 +0530

    Fix: Change catch-all route from '*' to '/*'

--- server.js
+++ server.js
@@ -36,7 +36,8 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'frontend/build')));
   
-  app.get('*', (req, res) => {
+  // FIX: Use '/*' instead of '*' for catch-all route
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
   });
 }
